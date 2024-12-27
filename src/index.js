9e8307a21e2d8f5a49feaa765e8f3f48677bea3f
Amit01999 <amitsarkar019999@gmail.com>
2024-12-27 18:28:35 +0600

    test error solve

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { RouterProvider } from 'react-router-dom';
 
 import router from './router/router';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,8 +11,3 @@ root.render(
   <RouterProvider router={router} />
   // </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
